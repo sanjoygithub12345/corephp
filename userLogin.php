b086720cf1fac47f1a651c031de5fbb3529a4e0f
sanjoy <sanjoysgg@gmail.com>
2020-01-18 21:51:57 -0800

    login page create

---
+++ userLogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>user login</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- bootstrap css -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!-- custom css -->
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	 <!-- fontawsome -->
+	 <link rel="stylesheet" type="text/css" href="css/all.min.css">
+</head>
+<body>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-md-6 offset-md-3">
+				<h3 class="text-primary text-center">online service management system</h3>
+				<form method="post">
+					<div class="form-group">
+					 <i class="fas fa-user"></i>
+					 <label>Email</label>	
+					 <input type="email" name="email" class="form-control">
+					</div>
+					<div class="form-group">
+					<i class="fas fa-envelope-square"></i>
+					 <label>password</label>
+					 <input type="password" name="password" class="form-control">
+					</div>
+					<button class="btn btn-primary">login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+</body>
+</html>
